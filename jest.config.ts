a12335dd5357805c9c1ab245c1c9e1b64a1381d3
robert-virtual <robertocastill63@gmail.com>
2025-08-08 22:32:03 -0600

    setup jest

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'jest';
+import { createCjsPreset } from 'jest-preset-angular/presets';
+import { pathsToModuleNameMapper } from 'ts-jest';
+
+import { compilerOptions } from './tsconfig.json';
+
+export default {
+  ...createCjsPreset({
+    testEnvironment: 'jest-preset-angular/environments/jest-jsdom-env',
+  }),
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>',
+  }),
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+} satisfies Config;
